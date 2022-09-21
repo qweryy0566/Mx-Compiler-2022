eda5f4d6d507c0ce53f0df61692215d572469957
qweryy0566 <qweryy0566@qq.com>
2022-09-21 18:02:30 +0800

    test: learning java

---
+++ src/Compiler.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Compiler {
+  public static int main(String[] args) {
+    int n, e;
+    Scanner scan = new Scanner(System.in);
+    n = scan.nextInt();
+    e = scan.nextInt();
+    scan.close();
+    System.out.println(n + e);
+    return 0;
+  }
+}
