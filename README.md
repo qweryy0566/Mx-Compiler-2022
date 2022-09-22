b8527041e44bf974e33eb3ab0ed8b6a37d25f9a0
qweryy0566 <qweryy0566@qq.com>
2022-09-22 16:32:34 +0800

    test: submit to OJ

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mx* Compiler
+
+## Semantics
--- src/Compiler.java
+++ src/Compiler.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Compiler {
   public static int main(String[] args) {
-    int n, e;
-    Scanner scan = new Scanner(System.in);
-    n = scan.nextInt();
-    e = scan.nextInt();
-    scan.close();
-    System.out.println(n + e);
-    return 0;
+    // int n, e;
+    // Scanner scan = new Scanner(System.in);
+    // n = scan.nextInt();
+    // e = scan.nextInt();
+    // scan.close();
+    // System.out.println(n + e);
+    return 1;
   }
 }
